ba5614d43b06a79faa31e230d5c24ef533a4d8e6
Jay Bhatt <bhattjay114@gmail.com>
2022-05-25 00:53:53 +0530

    gridcheck02

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ function App() {
     <div class="container">
       <div class="row">
         <div class="col-sm-12 col-md-6 col-lg-3">
-          <div class="card">
-            <div class="card-body box1">
+          <div class="card box1">
+            <div class="card-body ">
               <h5 class="card-title">Waze for Cities</h5>
               <p class="card-text">Let's work together to improve traffic and safety in your city.</p>
               <a href="#" class="btn btn-primary">Learn More</a>
@@ -16,8 +16,8 @@ function App() {
         </div>
 
         <div class="col-sm-12 col-md-6 col-lg-3">
-          <div class="card">
-            <div class="card-body box2">
+          <div class="card box2">
+            <div class="card-body ">
               <h5 class="card-title">Product Partners</h5>
               <p class="card-text">Pair Waze with your audio service, transportation app, car, or product.</p>
               <a href="#" class="btn btn-primary">Learn More</a>
@@ -26,8 +26,8 @@ function App() {
         </div>
 
         <div class="col-sm-12 col-md-6 col-lg-3">
-          <div class="card">
-            <div class="card-body box3">
+          <div class="card box3">
+            <div class="card-body ">
               <h5 class="card-title">Advertisers</h5>
               <p class="card-text">Reach customers when they're driving by or searching for businesses like yours.</p>
               <a href="#" class="btn btn-primary">Learn More</a>
@@ -36,8 +36,8 @@ function App() {
         </div>
 
         <div class="col-sm-12 col-md-6 col-lg-3">
-          <div class="card">
-            <div class="card-body box4">
+          <div class="card box4">
+            <div class="card-body ">
               <h5 class="card-title">Carpool Partners</h5>
               <p class="card-text">Upgrade the way your team gets to work with Waze Carpool.</p>
               <a href="#" class="btn btn-primary">Learn More</a>
